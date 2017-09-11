eaf8863aa2614568992c037e3c209560748b7f88
wim <wfjvdham@gmail.com>
2017-09-11 18:23:25 -0500

    combined stats basic and bayes, added google questen there

--- 03_stats_basics/stats_basic.R
+++ 03_stats_basics/stats_basic.R
@@ -1,3 +1,9 @@
+library(tidyverse)
+library(titanic)
+
+train = as_data_frame(titanic_train)
+test = as_data_frame(titanic_test)
+
 #seed
 set.seed(5)
 #random numbers
@@ -92,3 +98,24 @@ ggplot(crabs) +
 #-3.30476 + 0.16405Wi
 exp(-3.30 + 0.164 * 25)
 exp(-3.30 + 0.164 * 30)
+
+library(e1071)
+
+# titanic - model - Naive Bayes classifier
+# los proximos modelos no functionan con columnas de typo character (= string)
+# nosotros nesecitamos cambiar esos a factores o remover esas columnas
+train_factor <- train %>%
+  mutate(Survived = factor(Survived),
+         Sex = factor(Sex),
+         Embarked = factor(Embarked)) %>%
+  select(-Name, -Ticket, -Cabin)
+
+classifier <- naiveBayes(Survived ~ Sex + Age, train_factor)
+
+print(classifier)
+
+train_factor <- train_factor %>%
+  mutate(predSurvived = predict(classifier, train_factor, type = "class"))
+
+table(train_factor$predSurvived, train_factor$Survived)
+sum(train_factor$predSurvived == train_factor$Survived) / nrow(train_factor)
