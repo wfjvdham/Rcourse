25d7f6adc15270280a08d35381cb2e9197db9f89
wim <wfjvdham@gmail.com>
2017-09-14 21:48:59 -0500

    started shiny

---
+++ 17_shiny/02_slider_two_inputs.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+              label = "Choose a number", 
+              value = 25, min = 1, max = 100),
+  textInput(inputId = "title", 
+            label = "Write a title",
+            value = "Histogram of Random Normal Values"),
+  plotOutput("hist")
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num), main = input$title)
+  })
+}
+
+shinyApp(ui = ui, server = server)
