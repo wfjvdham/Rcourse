9ecc1299bd93d7606f9dec071d53ce5c837554b0
wim <wfjvdham@gmail.com>
2017-11-02 08:39:20 -0500

    use rows to put thinks next to eachother

--- 16_shiny/10_row_layout.R
+++ 16_shiny/10_row_layout.R
@@ -2,14 +2,10 @@ library(shiny)
 
 ui <- fluidPage(
   fluidRow(
-    column(3),
     column(5, sliderInput(inputId = "num", 
                           label = "Choose a number", 
-                          value = 25, min = 1, max = 100))
-  ),
-  fluidRow(
-    column(4, offset = 8,
-           plotOutput("hist")
+                          value = 25, min = 1, max = 100)),
+    column(4, plotOutput("hist")
     )
   )
 )
