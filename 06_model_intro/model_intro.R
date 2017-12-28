bf45135d3ba28656a4c4461bc75f4ea13ee8e16f
wim <wfjvdham@gmail.com>
2017-12-27 19:27:26 -0500

    add inference based on simulation and normal

--- 06_model_intro/model_intro.R
+++ 06_model_intro/model_intro.R
@@ -1,4 +1,58 @@
 library(tidyverse)
+set.seed(1)
+# Foundation for inference
+
+results <- data_frame(
+  cat = c("control", "treatment"),
+  buy = c(56, 41),
+  not_buy = c(19, 34)
+)
+results
+
+p_not_buy_c <- 19 / (19 + 56)
+p_not_buy_t <- 34 / (34 + 41)
+diff <- p_not_buy_t - p_not_buy_c
+
+# What is the probability that this difference is due to chance?
+
+n_total <- sum(results$not_buy, results$buy)
+p_buy <- sum(results$buy) / n_total
+
+# simulate using this p
+
+n <- 1000
+p_sim <- seq(0, 0, n)
+for(i in 1:n) {
+  sim_not_buy <- sample(c(0,1), 53, replace = TRUE)
+  n_not_buy_t <- sum(sim_not_buy)
+  n_not_buy_c <- 53 - n_not_buy_t
+  p_sim[i] <- n_not_buy_t / 75 - n_not_buy_c / 75
+}
+p_sim_df <- as_data_frame(p_sim)
+
+ggplot(p_sim_df) +
+  geom_histogram(aes(value)) +
+  geom_vline(xintercept = diff)
+
+sum(p_sim_df >= diff) / n * 100
+
+# Applying the normal model
+
+SE = 0.078 # Later we will explain how to obtain this value
+
+x <- seq(-0.3, 0.3, by = .01)
+
+ggplot() +
+  geom_histogram(aes(value), p_sim_df) +
+  geom_vline(xintercept = diff) +
+  geom_point(aes(x, dnorm(x, mean = 0, sd = SE) * 23), color = "red")
+
+Z = (0.2 - 0) / SE
+
+(1 - pnorm(Z, mean = 0, sd = 1)) * 100
+
+# p-values are smaal so H0 is rejected
+
 library(titanic)
 train = as_data_frame(titanic_train)
 test = as_data_frame(titanic_test)
