20821b0c0385fb884a93b6ba6d523ccd5bce655c
wim <wfjvdham@gmail.com>
2018-01-14 16:07:01 -0500

    add mongo example

--- 13_big_data/big_data.R
+++ 13_big_data/big_data.R
@@ -75,3 +75,36 @@ ggplot(scores, aes(gain, pred)) +
   geom_abline(intercept = 0, slope = 1, alpha = 0.15, color = 'blue') +
   geom_text(aes(label = substr(description, 1, 20)), size = 4, alpha = 0.75, vjust = -1) +
   labs(title='Average Gains Forecast', x = 'Actual', y = 'Predicted')
+
+# Connect to local example mongo db
+# https://docs.mongodb.com/getting-started/shell/import-data/
+# sudo service mongod start
+# mongoimport --db test --collection restaurants --drop --file ./primer-dataset.json
+library(mongolite)
+con <- mongo("restaurants", url = "mongodb://localhost/test")
+restaurant <- con$find('{"restaurant_id": "30075445"}')
+
+pipeline <- '[
+  {
+    "$project" : 
+    {
+      "latitude"  : {
+        "$arrayElemAt" : [ "$address.coord", 1 ]
+      },
+      "longitude" : {
+        "$arrayElemAt" : [ "$address.coord", 0 ]
+      },
+      "date" : {
+        "$arrayElemAt" : [ "$grades.date", 0 ]
+      },
+      "grade" : {
+        "$arrayElemAt" : [ "$grades.date", 1 ]
+      },
+      "score" : {
+        "$arrayElemAt" : [ "$grades.date", 2 ]
+      }
+    }
+  }
+]'
+
+unwind_restaurants <- con$aggregate(pipeline)
