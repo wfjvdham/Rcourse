c05755fbef59a405d82107722aa0c8b50831e1a9
Wim <wfjvdham@gmail.com>
2019-06-08 21:07:10 +0200

    put dica model as extra practive

--- Advanced/answers/03_map_model.R
+++ Advanced/answers/03_map_model.R
@@ -1,24 +1,18 @@
 library(tidyverse)
-library(aod)
 
 df <- tibble(
-  "jaar" = as.factor(1:10 + 2000),
-  "regio" = as.factor(rep(c("AMS", "UTR"), 5)),
+  "jaar" = 1:10 + 2000,
+  "regio" = rep(c("AMS", "UTR"), 5),
   "chemneo" = runif(10)
 )
 
 check_variable <- function(col_name) {
-  table(df[[col_name]], useNA = "always")
   from <- as.formula(paste("chemneo ~", col_name))
   model <- glm(from, data = df, family = binomial(link = 'logit'))
-  exp(coef(model))
   confint_list <- exp(confint(model))
-  summary(model)
-  terms = length(unique(df[[col_name]]))
-  wald_test <- wald.test(b = coef(model), Sigma = vcov(model), Terms = terms)
   list(
     "confint" = confint_list,
-    "H0" = wald_test$H0
+    "sum_model" = summary(model)
   )
 }
 
