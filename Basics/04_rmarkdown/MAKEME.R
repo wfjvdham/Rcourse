33cea5e2db0db513455b117c6e1df1ad4fca9858
Wim <wfjvdham@gmail.com>
2019-08-04 10:04:56 +0200

    Finish chapter rmarkdown

---
+++ Basics/04_rmarkdown/MAKEME.R
@@ -0,0 +1,6 @@
+# Make a html_notebook that looks nice with the following elements:
+# - Load some packages
+# - One question from a previous chapter
+# - A list of fruits
+# - A piece of code that you only show but do not run
+# - A link to your organisation website
